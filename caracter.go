32c4baed49d476f3925e8d0d657fbaca6396dd47
louis <louis.schoepf@ynov.com>
2025-09-11 15:08:13 +0200

    Tâche 1 Création personnage

---
+++ caracter.go
@@ -0,0 +1,14 @@
+package main
+
+import ("fmt")
+
+// Tâche 1 : Création du personnage 
+type Character struct 
+	Name string 
+	Classe string
+	Level int
+	Max_PV int 
+	PV int 
+	Inventory [10]string 
+
+	
